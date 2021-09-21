8b3728ae7984aa690c74b38603d42f4693d6a529
Bartosz Tomiak <bartosztomiak@Bartoszs-MacBook-Pro-2.local>
2021-09-21 23:09:31 +0200

    theme

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,91 @@
 <template>
-    <div>
-        
+  <header class="d-flex justify-content-between py-3 px-5 align-items-center">
+    <div class="d-flex align-items-center">
+      <g-image
+        alt="Example image"
+        src="~/assets/images/logo.svg"
+        class="me-5"
+        width="50"
+      />
+      <div class="d-none d-md-flex align-items-center">
+        <g-link to="/" class="text-dark me-4 text-decoration-none">Home</g-link>
+        <g-link to="/blog" class="text-dark me-4 text-decoration-none"
+          >Blog</g-link
+        >
+        <g-link to="/recipes" class="text-dark text-decoration-none"
+          >Recipes</g-link
+        >
+      </div>
     </div>
+    <div class="d-none d-md-flex aling-items-center">
+      <div class="d-flex flex-row aling-items-center">
+        <p class="d-flex align-items-center pe-3">Follow us</p>
+        <span class="d-flex align-items-center">|</span>
+        <a href="" class="d-flex align-items-center text-dark ms-3"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-youtube"
+          >
+            <path
+              d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"
+            ></path>
+            <polygon
+              points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"
+            ></polygon></svg
+        ></a>
+      </div>
+      <button class="btn btn-dark ms-4">Contact us</button>
+    </div>
+    <svg @click="toggleNav(true)" class="pointer d-block d-md-none feather feather-menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+    <div v-if="mobileNavVisible" class="mobile-nav d-block d-md-none position-absolute h-100 bg-dark">
+        <svg @click="toggleNav(false)" class="pointer text-white position-absolute feather feather-x" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+        <div class="d-flex flex-column align-items-end mt-5 pt-5 pe-5">
+            <g-link to="/" class="text-white mb-4 text-decoration-none">Home</g-link>
+            <g-link to="/blog" class="text-white mb-4 text-decoration-none"
+            >Blog</g-link
+            >
+            <g-link to="/recipes" class="text-white mb-4 text-decoration-none"
+            >Recipes</g-link
+            >
+        </div>
+    </div>
+  </header>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            mobileNavVisible: false,
+        }
+    },
+    methods: {
+        toggleNav(param) {
+            this.mobileNavVisible = param;
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.mobile-nav {
+    width: 380px;
+    max-width: 100%;
+    right: 0;
+    top: 0;
+
+    .feather-x {
+        z-index: 1;
+        right: 45px;
+        top: 30px;
+    }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,73 @@
 <template>
   <Layout>
-    <div class="container">
-      <g-image alt="Example image" src="~/assets/images/logo.svg" width="135" />
-      <h1>We are coming soon</h1>
-      <p>We are preparing something amazing and exciting for you.</p>
-    </div>
+    <section class="hero">
+      <div class="container">
+        <div class="row pb-5">
+          <div class="col-10">
+            <h1 class="mt-5 pt-5 text-start mb-3">
+              If you’re ready to change your relationship with food for the better, follow us @healthfirst for daily meal plans and updates.
+            </h1>
+            <p class="text-start mb-5 pb-5">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor adhuc invidunt.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="row py-5 my-5">
+          <div class="col-lg-6 d-flex flex-column align-items-start justify-content-center pe-5">
+            <h2 class="text-start mb-3">It's time to kick start a new lifestyle. Start eating, feeling & living better today. Build good habits with us in no time.</h2>
+            <p class="text-start">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna velum aliquyam erat, sed diam voluptua. At vero eos et mea accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est lorem ipsum.</p>
+            <!-- <button class="btn btn-dark mt-4">Contact us</button> -->
+          </div>
+          <div class="col-lg-6">
+            <g-image
+                alt="Example image"
+                src="~/assets/images/pasta.jpg"
+                class="me-5 img-fluid"
+              />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <h2 class="text-center mb-0">Latest on blog</h2>
+        <p class="mb-5">Lorem ipsum dolor sit amet</p>
+        <div class="row mb-5 pb-5">
+          <div v-for="post in 4" :key="post" class="col-sm-6 col-lg-3 d-flex flex-column align-itesm-start px-3">
+            <g-image
+                alt="Example image"
+                src="~/assets/images/pasta.jpg"
+                class="me-5 img-fluid"
+              />
+              <small class="category-badge align-self-start mt-3">DIET</small>
+              <h4 class="text-start mt-3">Fitness Recipes: Healthy Food for Any Workout</h4>
+              <p class="text-start text-small">Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh.</p>
+              <p class="text-start mt-3 border-bottom pb-2 align-self-start">Read more</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-dark">
+      <div class="container">
+        <div class="row py-5">
+          <div class="col-lg-6 py-5">
+            <g-image
+                alt="Example image"
+                src="~/assets/images/pasta.jpg"
+                class="me-5 img-fluid"
+              />
+          </div>
+          <div class="col-lg-6 ps-5 d-flex flex-column align-items-start justify-content-center">
+            <h2 class="mb-5 text-white text-start">Check out latest YouTube video:</h2>
+            <h3 class="text-start text-white">Zucchini pasta bolognese</h3>
+            <p class="text-start text-white">Lorem ipsum dolor sit amet ipsum dolor sit amet </p>
+            <button class="btn btn-success mt-5">Subscribe to our channel</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 </template>
 
