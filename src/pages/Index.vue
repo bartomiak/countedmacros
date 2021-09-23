d14fd85a736ae4b811605945ee7270955cbe7082
Bartosz Tomiak <btomiak@user.com>
2021-09-23 10:31:51 +0200

    colors

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <section class="hero">
       <div class="container">
         <div class="row pb-5">
-          <div class="col-10">
+          <div class="col-lg-10">
             <h1 class="mt-5 pt-5 text-start mb-3">
               If you’re ready to change your relationship with food for the better, follow us @healthfirst for daily meal plans and updates.
             </h1>
@@ -15,12 +15,13 @@
     <section>
       <div class="container">
         <div class="row py-5 my-5">
-          <div class="col-lg-6 d-flex flex-column align-items-start justify-content-center pe-5">
+          <div class="col-lg-5 d-flex flex-column align-items-start justify-content-center pe-5">
             <h2 class="text-start mb-3">It's time to kick start a new lifestyle. Start eating, feeling & living better today. Build good habits with us in no time.</h2>
             <p class="text-start">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna velum aliquyam erat, sed diam voluptua. At vero eos et mea accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est lorem ipsum.</p>
-            <!-- <button class="btn btn-dark mt-4">Contact us</button> -->
+            <button class="btn btn-dark mt-4">Read more about us</button>
+            
           </div>
-          <div class="col-lg-6">
+          <div class="col-lg-7">
             <g-image
                 alt="Example image"
                 src="~/assets/images/pasta.jpg"
@@ -41,7 +42,7 @@
                 src="~/assets/images/pasta.jpg"
                 class="me-5 img-fluid"
               />
-              <small class="category-badge align-self-start mt-3">DIET</small>
+              <small class="category-badge align-self-start mt-3 px-1">DIET</small>
               <h4 class="text-start mt-3">Fitness Recipes: Healthy Food for Any Workout</h4>
               <p class="text-start text-small">Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh.</p>
               <p class="text-start mt-3 border-bottom pb-2 align-self-start">Read more</p>
@@ -52,18 +53,18 @@
     <section class="bg-dark">
       <div class="container">
         <div class="row py-5">
-          <div class="col-lg-6 py-5">
+          <div class="col-lg-7 py-5">
             <g-image
                 alt="Example image"
                 src="~/assets/images/pasta.jpg"
                 class="me-5 img-fluid"
               />
           </div>
-          <div class="col-lg-6 ps-5 d-flex flex-column align-items-start justify-content-center">
+          <div class="col-lg-5 ps-5 d-flex flex-column align-items-start justify-content-center">
             <h2 class="mb-5 text-white text-start">Check out latest YouTube video:</h2>
             <h3 class="text-start text-white">Zucchini pasta bolognese</h3>
             <p class="text-start text-white">Lorem ipsum dolor sit amet ipsum dolor sit amet </p>
-            <button class="btn btn-success mt-5">Subscribe to our channel</button>
+            <button class="btn btn-success mt-5 py-2">Subscribe</button>
           </div>
         </div>
       </div>
